0139a365016ee915d7cc2ae07d3e4f1b3214c9f8
Jasmine <jazsaldano@gmail.com>
2026-08-23 15:02:47 -0700

    add average measure

--- experiments/lp_timing_experiment.py
+++ experiments/lp_timing_experiment.py
@@ -1,4 +1,5 @@
 import random
+import statistics
 
 from experiments.utils.graph_generator import random_graph
 from experiments.graphs.lp_shortest_path import run_lp
@@ -7,17 +8,32 @@ from experiments.graphs.lp_shortest_path import run_lp
 random.seed(42)
 
 sizes = [10, 20, 50, 100]
+runs = 5
 
 for n in sizes:
 
     G = random_graph(n)
 
-    objective, construction_time, solve_time, total_time = run_lp(
-        G,
-        measure_time=True
-    )
+    construction_times = []
+    solve_times = []
+    total_times = []
+
+    for _ in range(runs):
+
+        _, construction_time, solve_time, total_time = run_lp(
+            G,
+            measure_time=True
+        )
+
+        construction_times.append(construction_time)
+        solve_times.append(solve_time)
+        total_times.append(total_time)
+        
+    avg_construction = statistics.mean(construction_times)
+    avg_solve = statistics.mean(solve_times)
+    avg_total = statistics.mean(total_times)
 
     print("\nGraph Size:", n)
-    print("LP Construction:", construction_time)
-    print("LP Solve:", solve_time)
-    print("LP Total:", total_time)
+    print("Average LP Construction:", avg_construction)
+    print("Average LP Solve:", avg_solve)
+    print("Average LP Total:", avg_total)
