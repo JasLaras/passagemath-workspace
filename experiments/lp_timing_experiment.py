092fbc08f60facaa164f6929e20ed8e6580bf7e5
Jasmine <jazsaldano@gmail.com>
2026-09-07 14:12:41 -0700

    fixed lp_timing_experiment for testing

--- experiments/lp_timing_experiment.py
+++ experiments/lp_timing_experiment.py
@@ -2,7 +2,7 @@ import random
 import statistics
 
 from experiments.utils.graph_generator import random_graph
-from experiments.graphs.lp_shortest_path import run_lp
+from experiments.graphs.lp_shortest_path import build_lp, solve_lp
 
 
 random.seed(42)
@@ -27,11 +27,21 @@ for n in sizes:
 
         for _ in range(runs):
 
-            _, construction_time, solve_time, total_time = run_lp(
-            G,
-            solver=solver,
-            measure_time=True
+            # Measure LP construction
+            construction_start = time.perf_counter()
+
+            p, x = build_lp(G, solver=solver)
+
+            construction_end = time.perf_counter()
+
+            construction_time = (
+                construction_end - construction_start
             )
+
+            # Measure LP solve
+            _, solve_time = solve_lp(p, x, G)
+
+            total_time = construction_time + solve_time
 
             construction_times.append(construction_time)
             solve_times.append(solve_time)
@@ -41,9 +51,9 @@ for n in sizes:
         avg_solve = statistics.mean(solve_times)
         avg_total = statistics.mean(total_times)
 
-        solver_name = "Default" if solver is None else solver
+        # solver_name = "Default" if solver is None else solver
 
-        print("\nSolver:", solver_name)
+        print("\nSolver:", solver)
         print("\nGraph Size:", n)
         print("Average LP Construction:", avg_construction)
         print("Average LP Solve:", avg_solve)
